1aa2a58c398c093295cec50f155975b40bfafc0d
Monkiko <Monkiko425@gmail.com>
2021-09-04 11:09:23 -0500

    Added functions to play again

--- PythonExercises/NumberGuessing/NumberGuess.py
+++ PythonExercises/NumberGuessing/NumberGuess.py
@@ -15,6 +15,11 @@ def Clear():
         _ = system('clear')
 
 
+def Randomize():
+    global num
+    num = randrange(101)
+
+
 def Start():
     global response
     response = input("Guess a number between 1-100: ")
@@ -34,8 +39,10 @@ def InputCheck():
 
 def NumGuess():
     global guess
+    global num
     if guess == num:
         print("You guessed correctly. Good job!")
+        End()
     elif guess > num:
         print("OOPS! That was too high. Try again.")
         Start()
@@ -43,9 +50,22 @@ def NumGuess():
         print("OOPS! That was too low. Try again.")
         Start()
 
-num = randrange(101)
+def End():
+    answer = input("Would you like to play again? (yes/no) ")
+    if answer == "yes":
+        Clear()
+        Randomize()
+        Start()
+    elif answer == "no":
+        print("Thank you for playing!")
+    else:
+        print("Invalid response. Answer with yes or no.")
+        End()
+            
+
 
 print("Let's play a guessing game!")
 
 Clear()
+Randomize()
 Start()
