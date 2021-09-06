42628c0f4a8f385f27298e4fa06a294752e2e5bc
Monkiko <Monkiko425@gmail.com>
2021-09-05 21:04:25 -0500

    Added Functions, Corrected exiting windows

--- PythonExercises/NumberGuessing/NumGuessWin.py
+++ PythonExercises/NumberGuessing/NumGuessWin.py
@@ -2,25 +2,53 @@
 #
 # Created by: Ian Rivera-Leandry
 # Created on: 09-04-2021
-# Last Revised on: 09-04-2021
+# Last Revised on: 09-05-2021
 #
 import PySimpleGUI as sg
+from random import randrange
 #
 #
 sg.theme('DarkAmber')
 #
 #
-layout = [[sg.Text('Let\'s play a game! Guess a number between 1-100:'), sg.Text(size=(15,1), key='-OUTPUT-')], [sg.Input(key='-IN-')], [sg.Button('Submit'), sg.Button('Exit')]]
+def Randomize():
+    global num
+    num = randrange(101)
+#
+#
 
-window = sg.Window("Guess That Number!", layout)
+def Announcement():
+    layout = [[sg.Text('Let\'s play a number guessing game!'), sg.Text(size=(15,1))],
+                [sg.Button('OK'), sg.Button('Exit')]]
+    
+    window = sg.Window('Guess That Number!', layout)
 
-while True:  # Event Loop
     event, values = window.read()
-    print(event, values)
+    if event == 'OK':
+        window.close()
+        Start()
     if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Submit':
-        # Update the "output" text element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
-        
-window.close()
+        window.close()
+
+#
+#
+def Start():
+    layout = [[sg.Text('Guess a number between 1-100:'), sg.Text(size=(15,1), key='-OUTPUT-')],
+            [sg.Input(key='-IN-')],
+            [sg.Button('Submit'), sg.Button('Exit')]]
+
+    window = sg.Window("Guess That Number!", layout)
+
+    while True:  # Event Loop
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Submit':
+            # Update the "output" text element to be the value of "input" element
+            window['-OUTPUT-'].update(values['-IN-'])
+    window.close()
+#
+#
+Randomize()
+Announcement()
