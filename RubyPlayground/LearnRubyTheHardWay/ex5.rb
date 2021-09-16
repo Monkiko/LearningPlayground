a4be12e77ca4140efd8069f190b31ecf1f616a13
Monkiko <Monkiko425@gmail.com>
2021-09-16 10:14:32 -0500

    Added exercises

---
+++ RubyPlayground/LearnRubyTheHardWay/ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Ian Rivera-Leandry'
+my_age = 33  # as of April 2021
+my_height = 74 # inches
+my_weight = 280 # lbs
+my_eyes = 'Dark Brown'
+my_teeth = 'stained'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He weighs about #{my_weight} pounds."
+puts "He has been compared to a fridge."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are #{my_teeth} due to his diet and neglect."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight}."
