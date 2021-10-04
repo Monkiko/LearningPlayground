e0857fd67da208ef6f3e0b13c712dee4a3ab884f
Monkiko <Monkiko425@gmail.com>
2021-10-04 00:00:41 -0500

    Add more exercises

---
+++ PythonPlayground/LearnPythonTheHardWay/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Ian Rivera-Leandry'
+my_age = 33  # as of April 2021
+my_height = 74 # inches
+my_weight = 280 # lbs
+my_eyes = 'Dark Brown'
+my_teeth = 'stained'
+my_hair = 'Black'
+
+print ("Let's talk about %s." % my_name)
+print ("He's %d inches tall." % my_height)
+print ("He weighs about %d pounds." % my_weight)
+print ("He has been compared to a fridge.")
+print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
+print ("His teeth are %s due to his diet and neglect." % my_teeth)
+
+# this line is tricky, try to get it exactly right
+print ("If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
