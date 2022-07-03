e2ea68726463f9cef91d55c7f332a6397736aa8b
Monkiko <Monkiko425@gmail.com>
2022-07-03 10:22:51 -0500

    Initial Commit

---
+++ PythonPlayground/Challenges/LexicalScoped Challenges/Challenge2/LSCodeChallenge2.py
@@ -0,0 +1,19 @@
+#LCCS Round 2
+
+#Info:
+#This is the first part of a small challenge series done for the LexicalScoped Community
+#This is designed to practice and showcase our skills, while also demonstrating the diversity that can be found when multiple programmers attempt a single challenge.
+
+#Limitations
+#In order to keep the playground as simplified as possible we will be limiting things to the following:
+#Language: Python 3 (up to and including 3.10)
+#Operating System assumed will be Windows 10 (do not use system commands for other operating systems or you risk issues occurring)
+
+#Part 2 Objectives:
+
+#Without modifying the cards module you got from Round 1 results, create a High-Low-Same guessing game.
+
+#Must contain a Function to perform card comparison.
+#Must keep track of cards remaining and shuffle when we reach the end of the deck or shortly before (up to 5 cards before end of deck is acceptable)
+#Must validate input is correct, if incorrect must re-request input.
+#Must prompt after each round if you would like to continue playing or exit.
