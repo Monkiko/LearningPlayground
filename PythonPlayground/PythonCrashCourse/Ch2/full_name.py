aa9a45a06648035fcc569b4af6e3804f83f67f51
Monkiko <Monkiko425@gmail.com>
2023-04-20 22:07:37 -0500

    Initial Commit

---
+++ PythonPlayground/PythonCrashCourse/Ch2/full_name.py
@@ -0,0 +1,5 @@
+first_name = "ian"
+last_name = "rivera-leandry"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
---
+++ PythonPlayground/PythonCrashCourse/Ch2/name.py
@@ -0,0 +1,4 @@
+name = "ian rivera-leandry"
+print(name.title())
+print(name.upper())
+print(name.lower())
