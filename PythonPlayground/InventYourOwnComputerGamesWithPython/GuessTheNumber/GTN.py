e41f039bdd29284608ae4932170a54a1fd326864
Monkiko <Monkiko425@gmail.com>
2023-04-30 14:36:13 -0500

    Modified syntax around randint

--- PythonPlayground/InventYourOwnComputerGamesWithPython/GuessTheNumber/GTN.py
+++ PythonPlayground/InventYourOwnComputerGamesWithPython/GuessTheNumber/GTN.py
@@ -1,4 +1,5 @@
 # This is a Guess the Number game.
+
 from random import randint
 
 guessesTaken = 0
@@ -6,7 +7,7 @@ guessesTaken = 0
 print('Hello! What is your name?')
 myName = input()
 
-number = random.randint(1, 20)
+number = randint(1, 20)
 print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
 
 for guessesTaken in range(6):
