abe0b833136eb9838e1c68af67b874dc8c5befab
Monkiko <Monkiko425@gmail.com>
2024-01-30 07:37:09 -0600

    Added conditions to make new spelling list or read existing one

--- PythonPlayground/SpellingQuiz/spellingQuiz.py
+++ PythonPlayground/SpellingQuiz/spellingQuiz.py
@@ -2,24 +2,36 @@ import os
 
 choice = str(input("Do you want to create a new spelling list? Y/N: "))
 
-if choice == "Y":
+if choice == "Y" or choice == "y":
     
     if os.path.exists("spelling_list.txt"):
         os.remove("spelling_list.txt")
-    else:
-        print("The spelling_list.txt file does not exist")
     
     listLength = int(input("Please indicate the number of words in your spelling list: "))
 
     i = 0
-    spellingList = []
+    makeSpellingList = []
     while i < listLength:
-        spellingList.append(str(input("Provide a word for the spelling list: ")))
+        makeSpellingList.append(str(input("Provide a word for the spelling list: ")))
         i+=1
 
     file = open('spelling_list.txt', 'w')
 
-    for i in spellingList:
-        file.write(i)
+    for i in makeSpellingList:
+        file.write(f"{i}\n")
 
     file.close()
+
+elif choice == "N" or choice == "n":
+    spelling_file = open("spelling_list.txt", "r")
+
+    spellingList = []
+    spellingList = spelling_file.readlines()
+
+    spelling_file.close()
+
+    for i in spellingList:
+        print(i, end ="")
+
+else:
+    print("Incorrect Input. Please enter either 'Y' or 'N'")
