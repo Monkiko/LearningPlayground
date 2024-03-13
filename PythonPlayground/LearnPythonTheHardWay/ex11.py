4232132787d8cb524d276e1064e7d46df2639cbc
Monkiko <Monkiko425@gmail.com>
2024-03-12 21:25:56 -0500

    Initial Commit

---
+++ PythonPlayground/LearnPythonTheHardWay/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print("How much do you weigh?"),
+weight = input()
+
+print("So, you're %r old, %r tall and weigh %r pounds." % (age, height, weight))
---
+++ PythonPlayground/LearnPythonTheHardWay/ex12.py
@@ -0,0 +1,5 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("So, you're %r years old, %r inches tall, and weigh %r pounds." % (age, height, weight))
