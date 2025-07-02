eb2b508c02efec30cb4c8003064247e67280f4aa
Monkiko <Monkiko425@gmail.com>
2025-07-02 14:30:03 -0500

    Added socialMedia files

---
+++ C++Playground/socialMedia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string topics[] = {"Sports", "Politics", "Business", "Technology", "Art"};
+
+	int index;
+	cin >> index;
+
+	cout << topics[index];
+
+}
