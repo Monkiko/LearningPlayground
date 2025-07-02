4b25a198f30a165683adc9fb6a7a351abb778b64
Monkiko <Monkiko425@gmail.com>
2025-07-02 11:25:58 -0500

    Added countryCards files

---
+++ C++Playground/countryCards.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string country, capital;
+
+	cin >> country >> capital;
+
+	cout << "Country: " + country << endl;
+	cout << "Capital: " + capital;
+}
